be97aa3df7d30d43f2b82726c7ad58e33d151df3
ezterry <ezterry3@gmail.com>
2011-01-16 19:22:22 -0500

    Cleanup make file for DS use within this repo

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_MODULE := gps.$(TARGET_BOOTLOADER_BOARD_NAME)
+LOCAL_MODULE := gps.$(TARGET_GPSSHIM_BOARD_NAME)
 
 LOCAL_SHARED_LIBRARIES:= \
 	liblog \
@@ -17,7 +17,7 @@ LOCAL_SRC_FILES += \
 LOCAL_CFLAGS += \
     -fno-short-enums 
 
-ifneq ($(BOARD_GPS_BAD_AGPS),)
+ifneq ($(BOARD_GPSSHIM_BAD_AGPS),)
 LOCAL_CFLAGS += \
     -DNO_AGPS
 endif
@@ -31,5 +31,40 @@ LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
 include $(BUILD_SHARED_LIBRARY)
+
+#If ALT_GPSSHIM_BOARDNAME is set generate a second library
+# (aka trout if the main board name was sapphire)
+ifneq ($(ALT_GPSSHIM_BOARDNAME),)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE := gps.$(ALT_GPSSHIM_BOARDNAME)
+
+LOCAL_SHARED_LIBRARIES:= \
+    liblog \
+    $(BOARD_GPS_LIBRARIES) 
+
+LOCAL_SRC_FILES += \
+    gps.c
+
+LOCAL_CFLAGS += \
+    -fno-short-enums
+
+ifneq ($(BOARD_GPSSHIM_BAD_AGPS),)
+LOCAL_CFLAGS += \
+    -DNO_AGPS
+endif
+
+LOCAL_CFLAGS += \
+    -DNEEDS_INITIAL_XTRA
+
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif #ALT_GPSSHIM_BOARDNAME set
 
 endif # BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
